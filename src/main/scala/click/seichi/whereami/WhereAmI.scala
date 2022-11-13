66c21cf67815fa59b416a392602cea5a9e0a5473
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-13 16:09:00 +0900

    feat: add onEnable

--- src/main/scala/click/seichi/whereami/WhereAmI.scala
+++ src/main/scala/click/seichi/whereami/WhereAmI.scala
@@ -1,9 +1,20 @@
 package click.seichi.whereami
 
+import click.seichi.whereami.commands.{Reload, ShowMap}
+import click.seichi.whereami.config.MapDomain
 import org.bukkit.plugin.java.JavaPlugin
 
 class WhereAmI extends JavaPlugin {
   override def onEnable(): Unit = {
+    implicit val instance: WhereAmI = this
+
+    saveDefaultConfig()
+    MapDomain.load()
+
+    Map("whereami" -> Reload(), "map" -> ShowMap()).foreach {
+      case (commandName, executor) => instance.getCommand(commandName).setExecutor(executor)
+    }
+
     this.getLogger.info("WhereAmI is enabled.")
   }
 }
