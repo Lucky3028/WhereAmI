97c5371026fee9018dc50e9fcf812910888a5ae8
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-11-13 16:08:31 +0900

    feat: add MapDomain

---
+++ src/main/scala/click/seichi/whereami/config/MapDomain.scala
@@ -0,0 +1,22 @@
+package click.seichi.whereami.config
+
+import org.bukkit.plugin.java.JavaPlugin
+
+object MapDomain {
+  private val configPath = "map-domain"
+  private val errMessage = "The config value ('map-domain') is not set."
+  private var value: Option[String] = None
+
+  def load()(implicit instance: JavaPlugin): Unit = {
+    val newValue = Option(instance.getConfig.getString(configPath))
+    require(newValue.isDefined, errMessage)
+
+    value = newValue
+  }
+
+  def get(): Option[String] = {
+    require(value.isDefined, errMessage)
+
+    value
+  }
+}
